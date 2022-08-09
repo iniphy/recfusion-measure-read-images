e72c71c781f59dacfd4c33baf5b0a4fe84b2d40b
quepas <kiepas.patryk@gmail.com>
2022-08-09 11:12:44 +0200

    Fixing compilation errors

--- main.cpp
+++ main.cpp
@@ -1,36 +1,33 @@
-#define RF_220
 #include "timer.hpp"
-#include <iostream>
 
 #include <RecFusion.h>
 #include <vector>
-#include <memory>
 
 using namespace RecFusion;
 
 int main() {
     const int max_num_read_images = 100;
-    int num_sensor;
+    int num_sensor = 0;
 
 #ifdef RF_136
     std::cout << "Using RecFusion 1.3.6 backend" << std::endl;
     Sensor query_sensor;
     num_sensor = query_sensor.deviceCount();
-#elifdef RF_220
-    RecFusionSDK::init();
+#elif RF_220
     std::cout << "Using RecFusion 2.2.0 backend" << std::endl;
+    RecFusionSDK::init();
     SensorManager sensor_manager;
     num_sensor = sensor_manager.deviceCount();
 #endif
 
     // Opening all available sensors
-    std::vector<std::unique_ptr<Sensor>> sensors(num_sensor);
+    std::vector<Sensor*> sensors(num_sensor);
     for (int sensor_id = 0; sensor_id < num_sensor; ++sensor_id) {
 #ifdef RF_136
-        sensors.push_back(std::make_unique<Sensor>());
+        sensors.push_back(new Sensor());
         sensors[sensor_id]->open(sensor_id);
-#elifdef RF_220
-        sensors.push_back(sensor_manager.sensor(sensor_id))
+#elif RF_220
+        sensors.push_back(sensor_manager.sensor(sensor_id));
         sensors[sensor_id]->open();
 #endif
     }
@@ -49,7 +46,7 @@ int main() {
     for (int capture_id = 0; capture_id < 100; ++capture_id) {
         for (int sensor_id = 0; sensor_id < num_sensor; ++sensor_id) {
             timer.reset();
-            sensors[sensor_id]->readImages(*depth_img, *color_img);
+            sensors[sensor_id]->readImage(*depth_img, *color_img);
             times[sensor_id].push_back(timer.elapsed());
         }
     }
@@ -59,7 +56,11 @@ int main() {
         sensors[sensor_id]->close();
     }
 
-#ifdef RF_220
+#ifdef RF_136
+    for (int sensor_id = 0; sensor_id < num_sensor; ++sensor_id) {
+        delete sensors[sensor_id];
+    }
+#elif RF_220
     RecFusionSDK::deinit();
 #endif
 }
